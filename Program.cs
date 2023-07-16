db0f869a065e03b8526d59427001db334246659f
JohnnySenior <jamshidbektursunboev@gmail.com>
2023-07-16 23:28:33 +0900

    Calculate Area Of Circle

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("This console app calculate the area of Circle");
+
+Console.Write("Enter the radius: ");
+var radius = double.Parse(Console.ReadLine());
+var pi = Math.PI;
+
+Console.WriteLine($"Area: {pi} * ({radius} * {radius}) = {pi * Math.Pow(radius, 2)}");
