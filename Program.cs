7f1433c697ce4bd0ea6df89a7cda76ab6f87c695
JohnnySenior <jamshidbektursunboev@gmail.com>
2023-07-19 14:47:02 +0900

    Add-Const-ForPi

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 Console.Write("Enter the radius: ");
 double radius = double.Parse(Console.ReadLine());
-double pi = Math.PI;
+const double pi = Math.PI;
 
 Console.WriteLine($"Area: {pi} * ({radius} * {radius}) = {pi * Math.Pow(radius, 2)}");
 
