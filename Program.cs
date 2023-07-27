9d53597115c7d62ff162f69aa9d0f18e5e58ad25
JohnnySenior <jamshidbektursunboev@gmail.com>
2023-07-27 14:20:09 +0900

    Add-doWhile-Loop

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿Console.WriteLine("This console app calculate the area of shapes as follow:");
+﻿Console.WriteLine("This console app calculate the area check your knowlage of Geometry.");
+Console.WriteLine("App give you values, you have to find area !");
+Console.WriteLine("Note: Area(result) will be rounded !");
 Console.WriteLine();
 Console.WriteLine("1. Circle");
 Console.WriteLine("2. Triangle");
 Console.WriteLine();
 Console.Write("Choose shape: ");
+
+Random random = new Random();
 
 string stringOption = Console.ReadLine();
 int option = int.Parse(stringOption);
@@ -11,18 +15,29 @@ int option = int.Parse(stringOption);
 switch (option)
 {
     case 1:
-        Console.Write("Enter the radius: ");
-        double radius = double.Parse(Console.ReadLine());
-        const double pi = Math.PI;
+        double radius = random.Next(1, 10);
+        const double pi = 3.14;
+        double area = pi * Math.Pow(radius, 2);
+        double roundedArea = Math.Round(area);
+        double userArea;
+        do
+        {
+            Console.WriteLine($"Radius: {radius}");
 
-        Console.WriteLine($"Area: {pi} * ({radius} * {radius}) = {pi * Math.Pow(radius, 2)}");
+            Console.Write($"Calculate the area: ");
+            userArea = double.Parse(Console.ReadLine());
+
+            if (roundedArea == userArea)
+                Console.WriteLine("Bingoo, You are awsome.");
+            else
+                Console.WriteLine("Wrong answer, you can do it, try again");
+        } while (roundedArea != userArea);
         break;
-
     case 2:
         Console.WriteLine("We should validate triangle.");
         Console.WriteLine("Because we use Heron's formula to calculate the area.");
 
-        Console.Write("First side: ");
+    key1: Console.Write("First side: ");
         double side1 = double.Parse(Console.ReadLine());
 
         Console.Write("Second side: ");
@@ -35,13 +50,16 @@ switch (option)
         {
             Console.WriteLine("Successfully validated !!!");
             double semiPerimetr = (side1 + side2 + side3) / 2;
-            double area = Math.Sqrt(semiPerimetr * (semiPerimetr - side1)
+            double areaTriangle = Math.Sqrt(semiPerimetr * (semiPerimetr - side1)
                                                  * (semiPerimetr - side2)
                                                  * (semiPerimetr - side3));
-            Console.WriteLine($"Area: {area}");
+            Console.WriteLine($"Area: {areaTriangle}");
         }
         else
-            Console.WriteLine("No such triangle exists.");
+        {
+            Console.WriteLine("No such triangle exists. Enter right numbers.");
+            goto key1;
+        }
         break;
 
     default:
